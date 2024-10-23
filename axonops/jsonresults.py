11cc5b64c4326dfee4e8420c679f93342a3dd354
Johnny Miller <johnny@digitalis.io>
2024-10-23 16:23:10 +0200

    feat: renaming dir

--- axonops/jsonresults.py
+++ axonops/jsonresults.py
@@ -15,7 +15,7 @@ def setup_json_data_directory():
     today = datetime.datetime.now()
     formatted_date = today.strftime('%Y%m%d')
     directory_name = f'{formatted_date}-{uuid.uuid4()}'
-    base_directory = 'data/jsonresults'
+    base_directory = 'data/results'
     new_directory_path = os.path.join(base_directory, directory_name)
 
     os.makedirs(new_directory_path, exist_ok=True)
