0c03600c23088bd8cd3f34c4d3ff6e4c6d7da73a
Johnny Miller <johnny@digitalis.io>
2024-10-28 10:27:08 +0100

    chore: logging leve at INFO

--- axonops/logger.py
+++ axonops/logger.py
@@ -15,14 +15,14 @@ import logging
 import sys
 
 
-def setup_logger(name='AxonOpsLogger', level=logging.DEBUG):
+def setup_logger(name='AxonOpsLogger', level=logging.INFO):
     # Create a custom logger
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
     # Create handlers
     console_handler = logging.StreamHandler(sys.stdout)
-    console_handler.setLevel(logging.DEBUG)
+    console_handler.setLevel(level)
 
     # Create formatters and add them to handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s')
